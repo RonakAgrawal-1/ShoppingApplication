34e278b9fb8097d1708f38601c2868e475f377fd
ronak <ronaka2328@gmail.com>
2023-06-13 17:48:30 +0530

    Initial commit

---
+++ order-service/src/test/java/com/shoppingapplication/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.shoppingapplication.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+}
---
+++ product-service/src/test/java/com/shoppingapplication/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.shoppingapplication.productservice;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class ProductServiceApplicationTests {
+
+}
