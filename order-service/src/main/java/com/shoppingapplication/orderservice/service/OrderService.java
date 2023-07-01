9406f0fbc618c62f35f9f4c8930e4f026aa159bb
ronak <ronaka2328@gmail.com>
2023-07-02 02:44:51 +0530

    Implemented notification service and kafka

--- order-service/src/main/java/com/shoppingapplication/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/shoppingapplication/orderservice/service/OrderService.java
@@ -1,12 +1,16 @@
 package com.shoppingapplication.orderservice.service;
 
+import brave.Span;
+import brave.Tracer;
 import com.shoppingapplication.orderservice.dto.InventoryResponse;
 import com.shoppingapplication.orderservice.dto.OrderLineItemsDto;
 import com.shoppingapplication.orderservice.dto.OrderRequest;
+import com.shoppingapplication.orderservice.event.OrderPlacedEvent;
 import com.shoppingapplication.orderservice.model.Order;
 import com.shoppingapplication.orderservice.model.OrderLineItems;
 import com.shoppingapplication.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,8 +26,10 @@ public class OrderService {
 
     private final WebClient.Builder webClientBuilder;
     private final OrderRepository orderRepository;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -34,21 +40,29 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        //Call inventory service, and place order if product is in stock
-        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
 
-        InventoryResponse[] inventoryResponses = webClientBuilder.build().get().
-                uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        try(Tracer.SpanInScope isLookup = tracer.withSpanInScope(inventoryServiceLookup.start())) {
+            //Call inventory service, and place order if product is in stock
+            List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+            InventoryResponse[] inventoryResponses = webClientBuilder.build().get().
+                    uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        if(Boolean.TRUE.equals(allProductsInStock)) {
-            orderRepository.save(order);
-        } else {
-            throw new IllegalArgumentException("Product is not in stock, please try again later");
+            boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+
+            if(Boolean.TRUE.equals(allProductsInStock)) {
+                orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+        }finally {
+            inventoryServiceLookup.flush();
         }
     }
 
